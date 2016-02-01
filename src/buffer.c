2420e35d2fe62e9ee296bd609ba5de25ab21e709
Adam Heins <mail@adamheins.com>
2016-02-01 11:26:41 -0500

    Enhanced buffer library.
    Added an init function, to initialized the buffer.
    Added a function to detect if a jump is present in the buffer. This can
    be used to detect discontinuities such as abrupt detection distance
    changes, signifying a landmark.

--- src/buffer.c
+++ src/buffer.c
@@ -1,10 +1,17 @@
 #include "buffer.h"
 
-// A nice little circular buffer for holding sensor readings.
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
 struct Buffer {
-  unsigned int index;
   int buffer[BUF_SIZE];
+  unsigned int index;
 };
+
+void buffer_init(Buffer *buf) {
+  memset(buf, 0, sizeof(Buffer));
+}
 
 void buffer_insert(Buffer *buf, int value) {
   buf->index = (buf->index + 1) % BUF_SIZE;
@@ -18,4 +25,31 @@ int buffer_avg(Buffer *buf) {
     sum += buf->buffer[i]; // NOTE we're making an assumption that overflow will not occur
   }
   return sum / BUF_SIZE;
+}
+
+bool buffer_has_jumped(Buffer *buf, int threshold) {
+  int count = 0;      // Track how many elements we've processed so far.
+  int half1_sum = 0;  // Sum of elements in the first half of the buffer.
+  int half2_sum = 0;  // Sum of elements in the second half of the buffer.
+
+  // Process current position to the end (less recent elements).
+  for (int i = buf->index; i < BUF_SIZE; ++i) {
+    if (count++ < BUF_SIZE / 2) {
+      half1_sum += buf->buffer[i];
+    } else {
+      half2_sum += buf->buffer[i];
+    }
+  }
+
+  // Process start to current position (more recent elements).
+  for (int i = 0; i < buf->index; ++i) {
+    if (count++ < BUF_SIZE / 2) {
+      half1_sum += buf->buffer[i];
+    } else {
+      half2_sum += buf->buffer[i];
+    }
+  }
+
+  // Check if the difference between the two halves exceeds the threshold.
+  return abs(half1_sum - half2_sum) * 2 / BUF_SIZE >= threshold;
 }
--- src/buffer.h
+++ src/buffer.h
@@ -6,6 +6,28 @@
 struct Buffer;
 typedef struct Buffer Buffer;
 
+// Initialize the buffer.
+//
+// @param buf A pointer to the buffer to initialize.
+void buffer_init(Buffer *buf);
+
+// Insert an element into the buffer.
+//
+// @param buf A pointer to the buffer.
+// @param value The value to insert into the buffer.
 void buffer_insert(Buffer *buf, int value);
 
+// Calculate the average value of the buffer.
+//
+// @param buf A pointer to the buffer.
+//
+// @return The average value of the buffer's contents.
 int buffer_avg(Buffer *buf);
+
+// Check if a jump has been detected in the buffer's values.
+//
+// @param buf A pointer to the buffer.
+// @param threshold The value the buffer must have jumped to register as a jump.
+//
+// @return True if the buffer has jumped, false otherwise.
+bool buffer_has_jumped(Buffer *buf, int threshold) {
