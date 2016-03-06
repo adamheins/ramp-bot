6aad2812e31041d046ca6fc49559c01019f726fe
Adam Heins <mail@adamheins.com>
2016-03-06 00:54:14 -0500

    Clean up.

--- drive/drive.ino
+++ drive/drive.ino
@@ -1,166 +1,13 @@
+#include <Infrared.h>
+#include <Pins.h>
 #include <Ultra.h>
+#include <Duration.h>
+#include <Drive.h>
+#include <Phase.h>
 
 #include <Servo.h>
 
-// NOTE: servo values range from 0 - 180 where:
-//  0 is full reverse
-//  90 is full stop
-//  180 is full forward
-
-// Left side moves somewhat faster than the right side, so we need to manually
-// slow it down.
-
-// Layout of the robot's servos:
-//
-// FL--FR
-// |    |
-// |    |
-// |    |
-// BL--BR
-
-// Delay on each iteration of the loop.
-#define PERIOD 1000
-
-// Value to write to servo to stop it.
-#define SERVO_STOP 90
-
-// Servo pins.
-#define FL_PIN 8
-#define FR_PIN 9
-#define BL_PIN 10
-#define BR_PIN 11
-
-#define PAN_PIN 8
-
-Servo pan_servo;
-
-Servo SERVOS[2][2];
-
-// This is an idea, maintain some state with the servos?
-// Would probably make more sense to just rely on sensor readings, so we are more or less stateless (aside from buffered values)
-typedef struct EverestServo {
-  Servo servo;
-  int dist;
-} EverestServo;
-
-typedef enum Side {
-  LEFT = 0,
-  RIGHT,
-} Side;
-
-typedef enum Bumper {
-  FRONT = 0,
-  BACK,
-} Bumper;
-
-// Initialize the servos.
-void servo_init(void) {
-  SERVOS[LEFT][FRONT].attach(FL_PIN);
-  SERVOS[LEFT][BACK].attach(BL_PIN);
-  SERVOS[RIGHT][FRONT].attach(FR_PIN);
-  SERVOS[RIGHT][BACK].attach(BR_PIN);
-}
-
-// Drive one side of the motor.
-//
-// @param side The side to turn {LEFt|RIGHT}
-// @param vel  Velocity at which to drive (0 is stopped).
-void servo_drive_side(int side, int vel) {
-  SERVOS[side][FRONT].write(SERVO_STOP + vel);
-  SERVOS[side][BACK].write(SERVO_STOP + vel);
-}
-
-// Drive the entire robot in one direction.
-//
-// @param vel The velocity at which to drive (positive means forward, negative
-// is backward).
-void servo_drive(int vel) {
-  servo_drive_side(LEFT, vel);
-  servo_drive_side(RIGHT, -vel);
-}
-
-// Turn the robot on the spot.
-//
-// Theoretically, if we write out the same value to both sides, the robot
-// should turn on the spot. Since each side turns at somewhat different speeds,
-// this may not be the case in practice.
-//
-// @param vel Speed of turning. Positive values turn clockwise, negative values
-// turn counter-clockwise.
-void servo_point_turn(int vel) {
-  servo_drive_side(LEFT, vel);
-  servo_drive_side(RIGHT, vel);
-}
-
-  //////////////
- // Duration //
-//////////////
-
-typedef struct Duration {
-  unsigned long start;
-  unsigned long current;
-} Duration;
-
-void duration_init(struct Duration *dur) {
-  dur->start = millis();
-  dur->current = dur->start;
-}
-
-unsigned long duration_get_duration(struct Duration *dur) {
-  return dur->current - dur->start;
-}
-
-void duration_tick(struct Duration *dur) {
-  dur->current = millis();
-}
-
-// We track two durations at any one time.
-// The first is a total duration, running from the start of the course.
-// The second is a phase duration, which is the duration of the current phase of the course (eg. find ramp, climb ramp, find base)
-struct Duration *total_duration;
-struct Duration *phase_duration;
-
-int current_phase = 0;
-
-  /////////////
- // Utility //
-/////////////
-
-void everest_delay(unsigned long ms) {
-  delay(ms);
-  // Do we care that these will be *slightly* out-of-sync? Perhaps.
-  duration_tick(total_duration);
-  duration_tick(phase_duration);
-}
-
-  //////////
- // Main //
-//////////
-
-int ultraPin = 7;
-
-int pan_angle = 0;
-int pan_inc = 1;
-
-// Ultrasonic sensor.
-Ultra *ultra;
-
-int sec = 0;
-
-void setup() {
-  Serial.begin(9600);  
-  ultra = new Ultra(ultraPin);
-  pan_servo.attach(PAN_PIN);
-  pan_servo.write(pan_angle);
-}
-
-// Terminate the program by sitting in an endless loop.
-void stop() {
-  while(1);
-}
-
-// loop() should be the master state machine, which delegates out to sub-loops that govern specific tasks/phases.
-void loop() {
+/*
   // dank panning
   pan_angle += pan_inc;
   pan_servo.write(pan_angle);
@@ -169,23 +16,165 @@ void loop() {
   } else if (pan_angle <= 0) {
     pan_inc = 1;
   }
-  
-  // ultrasonic readings
-  int duration = ultra->ping();
-  int distance = ultra->distance();
-  Edge *edge = ultra->edge();
+*/
 
-  Serial.print(distance);
-  Serial.print(" ");
-  if (edge->side == EdgeRight) {
-    Serial.println("Right");
-  } else if (edge->side == EdgeLeft) {
-    Serial.println("Left");
-  } else {
-    Serial.println("None");
+#define LEFT_SERVO_CORR -5
+#define RIGHT_SERVO_CORR 0
+#define VELOCITY 15
+
+#define WALL_DISTANCE 10
+#define WALL_THRESHOLD 2
+
+Phase phase = PhaseThree;
+
+// Ultrasonic sensor.
+Ultra *ultra;
+Servo pan_servo;
+
+// Four-wheel drive system.
+FWDrive *fwdrive;
+
+// Duration tracker.
+Duration *dur;
+
+// Infrared sensors.
+Infrared *bottom_irs[2];
+Infrared *front_irs[2];
+
+// Terminate the program by detaching motors and sitting in endless loop.
+void stop() {
+  fwdrive->stop();
+  while(1) {
+    delay(60000); // 1 min
   }
-  Serial.println(edge->side);
-  free(edge);
+}
 
-  delay(50);
+void setup() {
+  Serial.begin(9600);  
+
+  ultra = new Ultra(ULTRASONIC_PIN);
+  pan_servo.attach(PAN_SERVO_PIN);
+  pan_servo.write(90);
+  
+  bottom_irs[LEFT] = new Infrared(BOTTOM_IR_LEFT_PIN);
+  bottom_irs[RIGHT] = new Infrared(BOTTOM_IR_RIGHT_PIN);
+  
+  front_irs[LEFT] = new Infrared(FRONT_IR_LEFT_PIN);
+  front_irs[RIGHT] = new Infrared(FRONT_IR_RIGHT_PIN);
+
+  dur = new Duration(0);
+
+  // Start the four-wheel drive system.
+  fwdrive = new FWDrive();
+  fwdrive->start();
+}
+
+// Phase 3 globals
+bool found_base_2 = false;
+int base2_dist = 0;
+int past_turn = 1; // 1 is right, -1 is left
+
+void loop() {
+  
+  if (phase == PhaseOne) {
+    
+    // Monitor wall distance.
+    ultra->ping();
+    int wall_dist = ultra->distance();
+    
+    bool too_close = wall_dist < WALL_DISTANCE - WALL_THRESHOLD;
+    bool too_far = wall_dist > WALL_DISTANCE + WALL_THRESHOLD;
+
+    if (too_close) {
+      fwdrive->left(VELOCITY + LEFT_SERVO_CORR)->right(VELOCITY * 3 / 4 + RIGHT_SERVO_CORR);
+    } else if (too_far) {
+      fwdrive->left(VELOCITY * 3 / 4 + LEFT_SERVO_CORR)->right(VELOCITY + RIGHT_SERVO_CORR);
+    } else {
+      fwdrive->left(VELOCITY + LEFT_SERVO_CORR)->right(VELOCITY + RIGHT_SERVO_CORR);
+    }
+    
+    // Monitor front distance.
+    front_irs[LEFT]->ping();
+    front_irs[RIGHT]->ping();
+    
+    int front_dist1 = front_irs[LEFT]->distance();
+    int front_dist2 = front_irs[RIGHT]->distance();
+    int front_dist = (front_dist1 + front_dist2) / 2;
+    
+    Serial.print(front_dist1);
+    Serial.print("  ");
+    Serial.print(front_dist2);
+    Serial.print("  ");
+    Serial.println(front_dist);
+    
+    delay(PHASE_ONE_DELAY);
+    
+  } else if (phase == PhaseTwo) {
+    
+    bottom_irs[LEFT]->ping();
+    bottom_irs[RIGHT]->ping();
+  
+    bool left_edge = (7468.9 * pow(bottom_irs[LEFT]->distance(), -1.374) > BOTTOM_IR_RAMP_THRESHOLD);
+    bool right_edge = (2797.1 * pow(bottom_irs[RIGHT]->distance(), -1.212) > BOTTOM_IR_RAMP_THRESHOLD);
+    
+    if (left_edge) {
+      fwdrive->left(VELOCITY + LEFT_SERVO_CORR)->right(VELOCITY * 3 / 4 + RIGHT_SERVO_CORR);
+    } else if (right_edge) {
+      fwdrive->left(VELOCITY * 3 / 4 + LEFT_SERVO_CORR)->right(VELOCITY + RIGHT_SERVO_CORR);
+    } else {
+      fwdrive->left(VELOCITY + LEFT_SERVO_CORR)->right(VELOCITY + RIGHT_SERVO_CORR);
+    }
+  
+    delay(PHASE_TWO_DELAY);
+    
+  } else {
+
+    ultra->ping();
+    int dist = ultra->distance();
+
+    if (!found_base_2) {
+      fwdrive->pivot(5);
+      if (dist < 50) {
+        EdgeSide edge = ultra->edge();
+        if (edge != EdgeNone) {
+           found_base_2 = true;
+        }
+        if (edge == EdgeRecent) {
+          base2_dist = ultra->recent();
+        } else if (edge == EdgeOld) {
+          base2_dist = ultra->old(); 
+        }
+      }
+    } else {
+      // Need to "edge-follow"
+      // This is tricky because the distance is constantly changing, and the edge itself is transient
+      EdgeSide edge = ultra->edge();
+      if (edge == EdgeRecent) {
+        
+      } else if (edge == EdgeOld) {
+        
+      } else {
+        if (dist < base2_dist) {
+          base2_dist = dist; 
+        } else {
+          Serial.println("ERROR"); 
+        }
+      }
+      if (dist > base2_dist) {
+        // turn toward base (right)
+      } else {
+        base2_dist = dist;
+      }
+      fwdrive->drive(10); // TODO need control to go in correct direction
+    }
+
+    // pivot robot
+    // monitor US for edges
+    // move forward when we find one
+    delay(PHASE_THREE_DELAY);
+  }
+
+  if (dur->tick()) {
+    stop();
+  }
 }
