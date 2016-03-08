f6f50ea4548f89d1936eed045d5dfb52740d7c62
Adam Heins <mail@adamheins.com>
2016-03-08 17:48:46 -0500

    Add calibration curves for front IR sensors.

--- libraries/Everest/Infrared.h
+++ libraries/Everest/Infrared.h
@@ -20,5 +20,9 @@ class Infrared {
 };
 
 // IRs are non-linear, they need to be scaled to get linear distance values.
+
 #define BOTTOM_IR_LEFT_SCALE(d) (7468.9 * pow((d), -1.374))
 #define BOTTOM_IR_RIGHT_SCALE(d) (2797.1 * pow((d), -1.212))
+
+#define FRONT_IR_LEFT_SCALE(d) (7846.4 * pow((d), -1.083))
+#define FRONT_IR_RIGHT_SCALE(d) (30928.0 * pow((d), -1.316))
